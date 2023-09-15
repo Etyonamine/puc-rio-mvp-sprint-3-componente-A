3cc5080b4b58a72ec6b9aae4a347f56d7514d744
ETYonamine <etyonamine@users.noreply.github.com>
2023-09-15 00:08:56 -0300

    ajuste da linguagem

--- source/src/components/modelo/ListaModelos.jsx
+++ source/src/components/modelo/ListaModelos.jsx
@@ -1,76 +1,79 @@
-import React, { useEffect, useState }from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Link, useParams } from 'react-router-dom';
-/*const columns = [
-   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-]; */
-
-
+import Box from '@mui/material/Box';
 
 const columns = [
-  {field: 'codigo', headerName: 'codigo', width: 70},
+  {
+    field: 'codigo',
+    headerName: 'codigo',
+    headerClassName: 'super-app-theme--header',
+    headerAlign: 'center',
+    align: 'center',
+    flex: 0.3,
+    minWidth: 50,
+    width: 70
+  },
   /* {field: 'marca', headerName: 'Marca', width:130}, */
-  {field: 'nome_marca', headerName: 'Marca', width:130},
-  {field: 'nome', headerName: 'Nome', width:130}
+  {
+    field: 'nome_marca', headerName: 'Marca', headerClassName: 'super-app-theme--header',
+    headerAlign: 'center', width: 300
+  },
+  {
+    field: 'nome', headerName: 'Nome', headerClassName: 'super-app-theme--header',
+    headerAlign: 'center', width: 300
+
+  },
 ];
 
 export default function ListaModelos() {
-  const [modelosEncontrados, setListaModelos] = useState([]);    
-  const rows =[];
-/*   const {id} = useParams(); */
+  const [modelosEncontrados, setListaModelos] = useState([]);
+  const rows = [];
+  /*   const {id} = useParams(); */
 
-  useEffect(()=>{ 
+  useEffect(() => {
     fetch(`${import.meta.env.VITE_URL_API_VEICULO}/modelos`)
-    .then(response => response.json())
-    .then(responseData =>
-      setListaModelos(responseData.lista))
-    .catch(error => console.error(error));
+      .then(response => response.json())
+      .then(responseData =>
+        setListaModelos(responseData.lista))
+      .catch(error => console.error(error));
 
-  },[]);
-  
+  }, []);
+  const localizedTextsMap = {
+    columnMenuUnsort: "não classificado",
+    columnMenuSortAsc: "Classificar por ordem crescente",
+    columnMenuSortDesc: "Classificar por ordem decrescente",
+    columnMenuFilter: "Filtro",
+    columnMenuHideColumn: "Ocultar",
+    columnMenuShowColumns: "Mostrar colunas"
+  };
   return (
+
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={modelosEncontrados}
-        getRowId={(row) => row.codigo}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+      <Box
+        sx={{
+          height: 300,
+          width: '100%',
+          '& .super-app-theme--header': {
+            backgroundColor: 'rgba(255, 7, 0, 0.55)',
           },
         }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
+      >
+        <DataGrid
+          sx={{ m: 2 }}          
+          rows={modelosEncontrados}
+          getRowId={(row) => row.codigo}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          localeText={localizedTextsMap}
+        />
+      </Box>
     </div>
   );
 }
